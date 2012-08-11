58bb0c85ba0e3cf1e39d30e87f95d8a821331fb5
Juan Walker <jpwalker@gmail.com>
2012-08-11 00:01:14 -0400

    Really working now

--- desktop/file_listing.py
+++ desktop/file_listing.py
@@ -7,8 +7,8 @@ class FileList:
     files = []
 
     def __init__(self, cache_filename):
-        self.cache_file = filename
-        self.load_from_file()
+        self.cache_file = cache_filename
+        self.load_from_cache()
 
     def add_directory(self, dir):
         self.directories.append(dir)
@@ -24,7 +24,7 @@ class FileList:
     def get_listing(self):
         if not self.files:
             self.update_listing()
-        return self.files.copy()
+        return list(self.files)
 
     def update_listing(self):
         self.files = []
@@ -37,15 +37,21 @@ class FileList:
                     file = self._file_record(dirpath, name, True)
                     self.files.append(file)
                     
+                    
+    def save_to_cache(self):
+		with open(self.cache_file, 'w') as f:
+			data = dumps({'files': self.files})
+			f.write(data)
+			
     def load_from_cache(self):
     	self.files = []
-    	with open(cache_file) as f:
-    		data = json.loads(f.read())
-    		self.files = data.get('files')
+    	try:
+    		with open(self.cache_file) as f:
+    			data = loads(f.read())
+    			self.files = data.get('files')
+    	except:
+    		pass
     			
-	def save_to_cache(self):
-		with open(cache_file, 'w') as f:
-			data = json.dumps({'files': self.files})
-			f.write(data)
+
 			
 	
