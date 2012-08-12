9a0f8bf54d2868b25f5e298330e79e126f870645
Juan Walker <jpwalker@gmail.com>
2012-08-11 20:49:44 -0400

    stuff

--- desktop/file_listing.py
+++ desktop/file_listing.py
@@ -1,50 +1,62 @@
 import os
 from json import dumps, loads
+from hashlib import md5
 
 class FileList:
     directories = []
     filters = []
-    files = []
+    files = {}
 
     def __init__(self, cache_filename):
         self.cache_file = cache_filename
         self.load_from_cache()
 
     def add_directory(self, dir):
-        self.directories.append(dir)
+    	if dir:
+	        self.directories.append(dir)
+    
+    def set_directories(self, dirs):
+    	self.directories = dirs or []
 
     def add_filter(self, filter):
-        self.filters.append(filter)
+    	if filter:
+	        self.filters.append(filter)
 
     def _file_record(self, dirpath, name, is_dir):
-        return {'full_path': os.path.join(dirpath, name),
+        path = os.path.join(dirpath, name)
+        # TODO: Actually use a hash of the file contents
+        hash = hashlib.md5(path).hexdigest()
+        return {'full_path': path,
                 'name': name,
-                'is_dir': is_dir}
+                'is_dir': is_dir,
+                'hash': hash}
 
     def get_listing(self):
         if not self.files:
             self.update_listing()
-        return list(self.files)
+        return list(self.files.values())
 
     def update_listing(self):
-        self.files = []
+        self.files = {}
         for dir in self.directories:
             for (dirpath, dirnames, filenames) in os.walk(dir):
                 for name in filenames:
                     file = self._file_record(dirpath, name, False)
-                    self.files.append(file)
+                    self.files[file['hash']] = file
                 for name in dirnames:
                     file = self._file_record(dirpath, name, True)
-                    self.files.append(file)
+                    self.files[file['hash']] = file
                     
-                    
+    def get_file_info(self, file_hash):
+    	return files.get(file_hash)
+    	             
     def save_to_cache(self):
 		with open(self.cache_file, 'w') as f:
 			data = dumps({'files': self.files})
 			f.write(data)
 			
     def load_from_cache(self):
-    	self.files = []
+    	self.files = {}
     	try:
     		with open(self.cache_file) as f:
     			data = loads(f.read())
--- desktop/fileserver.py
+++ desktop/fileserver.py
@@ -19,12 +19,13 @@ PING_PATH = "/ping"
 REGISTER_PATH = "/app/register"
 NEW_USER_PATH = "/user/new"
 LOGIN_PATH = "/user/login"
-CONFIG_FILE = "./config.json"
+GET_UPLOAD_URL_PATH = "/transfer/%s/start_upload"
+CONFIG_FILE = "./config.ini"
 
 paths = {}
 install_id = None
 
-config = None
+
 
 @app.route('/status')
 def get_status():
@@ -32,6 +33,7 @@ def get_status():
     status += "OK!\r\n"
     status += "Ping Interval: %s\r\n" % PING_TIME
     return status
+
 
 @app.route('/files/<path:path>')
 def get_file(path):
@@ -42,6 +44,7 @@ def get_file(path):
     except:
         return "File not found"
 
+
     def file_piece():
         data = f.read(BUFFER_SIZE)
         while data:
@@ -50,121 +53,143 @@ def get_file(path):
 
     return Response(file_piece(), mimetype='application/octet-stream')
 
+
 @app.route('/list')
 def get_listing():
     return '\r\n'.join(paths)
 
-def setup_test():
+class FileServer:
+
+  def setup_test(self):
     paths['file1'] = '/Users/juan/Desktop/IMG_0343.JPG'
     paths['file2'] = '/Users/juan/Desktop/LivingSocial Voucher.pdf'
 
 
-def get_url(path):
-    ret = "http://%s:%s%s" % (config['server_host'],
-                               config['server_port'],
-                               path)
+  def get_url(self, path):
+    print self.config.config
+    ret = "http://%s:%s%s" % (self.config.get('server_host'),
+                              self.config.get('server_port'),
+                              path)
     print ret
     return ret
 
-def ping_server():
-    server_message = urlopen("%s/%s" % (get_url(PING_PATH), config['install_id']))
-    data = json.loads(server_message.read())
+
+  def ping_server(self):
+    server_message = urlopen("%s/%s" % (self.get_url(PING_PATH), 
+                                        self.config.get('install_id')))
+    data = loads(server_message.read())
     command = data.get("command")
     if command == "get_file":
-        send_file(data['file_id'], data['transfer_id'])
+      self.send_file(data['file_id'], data['transfer_id'])
     elif command == 'test':
-        print "Test command received!!!!!"
-    
-        
-def send_file(file_id, transfer_id):
-    path = get_file_path(file_id)
-    if path:
-        start_new_thread(_send_file, path)
+      print "Test command received!!!!!"
 
-# TODO: Have this thread somehow communicate its status
-def _send_file(path, transfer_id):
-    print "Executing transfer (%s) with %s" % (transfer_id, path)
 
-def get_file_path(file_id):
-    return paths.get(file_id)
+  def send_file(self, file_hash, transfer_id):
+    resp = urlopen(self.get_url(GET_UPLOAD_URL_PATH % transfer_id),
+             urlencode({'user_token': self.config.get('user_token')})
+    resp = loads(resp.read())
     
-def hashed_password():
+    url = resp['url']
+    
+    file = self.file_list.get_file_info(file_hash)
+    if not file:
+      print "File not found for hash: " + file_hash
+      # TODO: Let the server know the file wasn't found
+    start_thread(self._send_file, (file['path'], url))
+
+  # TODO: Have this thread somehow communicate its status
+  def _send_file(path, url):
+    print "Executing transfer (%s) with %s" % (transfer_id, file['path'])
+    pass
+
+  def hashed_password(self):
     # TODO: should hash with something random
     d = md5()
-    d.update(config["password"] + "ABC432")
+    d.update(self.config.get("password") + "ABC432")
     return d.hexdigest()
 
-def new_user(username, password):
+
+  def new_user(self, username, password):
     #TODO: Ask user for credentials
-    config.set('username', username)
-    config.set('password', password)
-    resp = urlopen(get_url(NEW_USER_PATH),
-            urlencode({'username': config['username'],
-                       'password': hashed_password()}))
+    self.config.set('username', username)
+    self.config.set('password', password)
+    resp = urlopen(self.get_url(NEW_USER_PATH),
+            urlencode({'username': self.config.get('username'),
+                       'password': self.hashed_password()}))
     resp_dict = loads(resp.read())
     token = resp_dict.get('user_token')
     if token:
-        config.set('user_token', token)
+      self.config.set('user_token', token)
     else:
-        # TODO: Real exceptions
-        print ("Create user failed, got back: %s" % resp_dict)
-        return False
+      # TODO: Real exceptions
+      print ("Create user failed, got back: %s" % resp_dict)
+      return False
     return True
 
-def login():
-    if config.get('user_token'):
-        return
-    resp = urlopen(get_url(LOGIN_PATH),
-            urlencode({'username': config['username'],
-                       'password': hashed_password()}))
+
+  def login(self):
+    if self.config.get('user_token'):
+      return
+    resp = urlopen(self.get_url(LOGIN_PATH),
+            urlencode({'username': self.config['username'],
+                       'password': self.hashed_password()}))
     resp_dict = loads(resp.read())
     token = resp_dict.get('user_token')
     if token:
-        config.set('user_token') = token
-        print "user token: " + token
+      self.config.set('user_token', token)
+      print "user token: " + token
     else:
-        # this should throw a specific exception
-        raise Exception("Tried to login, got back %s as token" % resp_dict)
+      # this should throw a specific exception
+      raise Exception("Tried to login, got back %s as token" % resp_dict)
 
-def register_with_server():
-    login()
-    resp = urlopen(get_url(REGISTER_PATH),
-                   urlencode({'user_token': config['user_token'],
-                              'port': config['public_port']}))
+
+  def register_with_server(self):
+    self.login()
+    resp = urlopen(self.get_url(REGISTER_PATH),
+                   urlencode({'user_token': self.config.get('user_token'),
+                              'port': self.config.get('public_port')}))
     new_conf = loads(resp.read())
     print new_conf
-    config.update(new_conf)
+    for k, v in new_conf:
+     self.config.set(k, v)
 
-def get_install_id():
-    if not config.get('install_id'):
-        register_with_server()
-    return config.get('install_id')
 
-def ping_thread(args):
+  def get_install_id(self):
+    if not self.config.get('install_id'):
+      self.register_with_server()
+    return self.config.get('install_id')
+
+
+  def ping_thread(self, args):
     while True:
-        ping_server()
-        sleep(PING_TIME)
+      self.ping_server()
+      sleep(PING_TIME)
 
 
-def verify_config():
-    return config.get('install_id')
+  def verify_config(self):
+    return self.config.get('install_id')
 
 
-def first_time_registration():
+  def first_time_registration(self):
     # TODO: This needs to get the user's info somehow
-    new_user('wan', 'pass')
-    config.set('username', 'wan')
-    config.set('install_id', get_install_id())
-    config.save()
+    self.new_user('wan', 'pass')
+    self.config.set('username', 'wan')
+    self.config.set('install_id', self.get_install_id())
+    self.config.save()
 
 
-def setup_config():
-    config = Config(CONFIG_FILE)
+  def setup_config(self):
+    self.config = Config(CONFIG_FILE)
+    self.file_list = FileList()
+    self.file_list.set_directories(self.config.get('shared_dirs'))
     
 
 if __name__ == '__main__':
-    setup_config()
-    start_new_thread(ping_thread, (None,))
-    #start_new_thread(file_scan_thread, (None,))
-    # TODO: File scanning thread
-    app.run(host='0.0.0.0', port=int(config.get('local_port')))
+  fs = FileServer()
+  fs.setup_config()
+  fs.ping_thread((None,))
+  #start_new_thread(ping_thread, (None,))
+  #start_new_thread(file_scan_thread, (None,))
+  # TODO: File scanning thread
+  app.run(host='0.0.0.0', port=int(fs.config.get('local_port')))
